a1eb55918907f2cfafcf401c798e1aa36054022e
Abrahamruiz2020 <abrahamruizramos2020@gmail.com>
2022-06-23 13:03:54 -0400

    commit_libros

---
+++ src/main/java/com/emergentes/utiles/ConexionBD.java
@@ -0,0 +1,48 @@
+
+package com.emergentes.utiles;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class ConexionBD {
+    static String driver = "com.mysql.jdbc.Driver";
+    static String url = "jdbc:mysql://localhost:3306/bd_agenda";
+    static String usuario = "root";
+    static String password ="";
+    
+    Connection conn = null;
+
+    public ConexionBD() {
+        
+        try {
+        Class.forName(driver);
+        conn = DriverManager.getConnection(url, usuario, password);
+        if(conn != null){
+            System.out.println("Conexion OK"+ conn);
+        }} catch (SQLException ex) {
+            System.out.println("Error de SQL"+ ex.getMessage());
+       
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        
+        }
+    }
+        public Connection conectar(){
+        return conn;
+        }
+        public void desconectar(){
+            try{
+            conn.close();
+            }catch (SQLException ex){
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
+
